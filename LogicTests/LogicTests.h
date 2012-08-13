80eabd2829c0233528fc6a9ad0fae51598119f07
Adrian Bigland <a.d.bigland@gmail.com>
2012-08-13 15:43:24 +0100

    Initial Commit

---
+++ LogicTests/LogicTests.h
@@ -0,0 +1,13 @@
+//
+//  LogicTests.h
+//  LogicTests
+//
+//  Created by Adrian Bigland on 13/08/2012.
+//  Copyright (c) 2012 Adrian Bigland. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LogicTests : SenTestCase
+
+@end
---
+++ LogicTests/LogicTests.m
@@ -0,0 +1,32 @@
+//
+//  LogicTests.m
+//  LogicTests
+//
+//  Created by Adrian Bigland on 13/08/2012.
+//  Copyright (c) 2012 Adrian Bigland. All rights reserved.
+//
+
+#import "LogicTests.h"
+
+@implementation LogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTests");
+}
+
+@end
